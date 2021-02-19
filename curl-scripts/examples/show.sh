bb30e7b7e8d72644fe49342425bdf3dc1c9ffd4d
idayass <yassmine.idalene01@gmail.com>
2021-02-19 17:25:35 -0500

    change node version on package.json

--- curl-scripts/examples/show.sh
+++ curl-scripts/examples/show.sh
@@ -3,9 +3,8 @@
 API="http://localhost:4741"
 URL_PATH="/examples"
 
-curl "http://localhost:4741/books/6029c6335a8681517400f229" \
+curl "http://localhost:4741/books/602f00578000fb189da39ec4" \
   --include \
   --request GET \
-
 
 echo
